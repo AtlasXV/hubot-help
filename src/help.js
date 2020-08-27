1e3ac1389ae37f5640bf2428493cd9024e61ebb3
wenxi <okwenxi@gmail.com>
2020-08-27 12:12:19 +0800

    replace bot name

--- src/help.js
+++ src/help.js
@@ -22,7 +22,7 @@ module.exports = (robot) => {
         return
       }
     } else {
-      cmds = cmds.filter(cmd => cmd.match(new RegExp('^hubot', 'i')))
+      cmds = cmds.filter(cmd => cmd.match(new RegExp('^hubot', 'i'))).map(cmd => cmd.replace(/^hubot\s*/i, 'Bender'))
       if (cmds.length === 0) {
         msg.send(`No available commands`)
         return
